fd06d93cfd8dfc3daf052c802c34cca9ee3dc60e
crunchy89 <rocker.hunt@gmail.com>
2020-07-01 00:39:42 +0800

    tes

---
+++ precache-manifest.b3824a3369ceeef68f42bc98e26b0de1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "122d1191605810ea2617a0f33a20f7cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fcaae540990c4061037e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7284f614631be8123dce",
+    "url": "/static/js/2.69f1b82b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.69f1b82b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcaae540990c4061037e",
+    "url": "/static/js/main.4bc81b13.chunk.js"
+  },
+  {
+    "revision": "bbc90e96631f533aa7e2",
+    "url": "/static/js/runtime-main.9c7fa445.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
